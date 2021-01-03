3508e866b16859204f8d5711f1263e3d2996d110
burn-piano-island <burn_piano_island@outlook.com>
2021-01-03 00:33:35 -0800

    Added code to get game feeds, command implementation WIP

--- utils/constants.ts
+++ utils/constants.ts
@@ -14,9 +14,10 @@ export module Environment {
 export module Paths {
     export const API_HOST_URL: string = `https://statsapi.web.nhl.com`;
     export const API_PART: string = 'api/v1';
+    export const API_ENDPOINT = `${API_HOST_URL}/${API_PART}`;
     
     export module Get {
-        export const Schedule: string = `${API_HOST_URL}/${API_PART}/schedule`;
+        export const Schedule: string = `${API_ENDPOINT}/schedule`;
         export const ScheduleByDate:(startDate: string, endDate?: string) => string = 
             (start, end) => `${Paths.Get.Schedule}?startDate=${start}&endDate=${end || start}`;
 
@@ -26,27 +27,30 @@ export module Paths {
         export const TeamScheduleByDate :(team: string, startDate: string, endDate?: string) => string =
             (id, start, end) => `${Schedule}?teamId=${id}&startDate=${start}&endDate=${end || start}`;
 
-        export const Teams: string = `${API_HOST_URL}/${API_PART}/teams`;
+        export const Teams: string = `${API_ENDPOINT}/teams`;
         export const Team:(id: string) => string = (id) => `${Paths.Get.Teams}/${id}`;
 
-        export const Divisions: string = `${API_HOST_URL}/${API_PART}/divisions`;
+        export const Divisions: string = `${API_ENDPOINT}/divisions`;
         export const Division: (id: string) => string =
             (id) => `${Paths.Get.Divisions}/${id}`;
 
-        export const Conferences: string = `${API_HOST_URL}/${API_PART}/conferences`;
+        export const Conferences: string = `${API_ENDPOINT}/conferences`;
         export const Conference: (id: string) => string =
             (id) => `${Paths.Get.Conferences}/${id}`;
         
-        export const People: string = `${API_HOST_URL}/${API_PART}/people`;
+        export const People: string = `${API_ENDPOINT}/people`;
         export const Person:(id: string) => string =
             (id) => `${Paths.Get.People}/${id}`;
 
-        export const Seasons: string = `${API_HOST_URL}/${API_PART}/seasons`;
+        export const Seasons: string = `${API_ENDPOINT}/seasons`;
         
         export const TeamStats: (id: string) => string = (id) => `${Team(id)}/stats`;
 
-        export const Standings: string = `${API_HOST_URL}/${API_PART}/standings`;
+        export const Standings: string = `${API_ENDPOINT}/standings`;
         export const CustomStandings: (type: StandingsTypes) => string = (type) => `${Standings}/${type}`;
+
+        export const GameFeed: (id: string) => string = (id) => `${API_ENDPOINT}/game/${id}/feed/live`;
+        export const GameFeedDiff: (id: string, timecode: string) => string = (id, timecode) => `${API_ENDPOINT}/game/${id}/feed/live/diffPatch?startTimecode=${timecode}`;
     }
 
     
