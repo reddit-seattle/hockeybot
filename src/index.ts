ffc3305f9bbbab40fe39af8eea232e4846134f6b
burn_piano_island <36089446+burn-piano-island@users.noreply.github.com>
2021-07-30 11:53:40 -0700

    Added some channel IDs to constants

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { GetLastGamesForTeam, GetNextGamesForTeam, GetSchedule, GetScores } from
 import { GetStandings } from './commands/StandingsCommands';
 import { GetTeamStats } from './commands/TeamCommands';
 import { Command } from './models/Command';
-import { Config, Environment } from './utils/constants';
+import { ChannelIds, Config, Environment } from './utils/constants';
 import { GetMessageArgs } from './utils/helpers';
 
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
@@ -49,7 +49,7 @@ client.on('ready', async () => {
     if (Environment.DEBUG) {
         //try to announce to servers when you go online
         client.guilds.cache.array().forEach((guild: Guild) => {
-            const debugChannel = guild.channels.cache.find(channel => channel.name == 'debug') as TextChannel;
+            const debugChannel = guild.channels.cache.find(ch => ch.id == ChannelIds.DEBUG) as TextChannel;
             debugChannel?.send("HockeyBot, reporting for duty!");
         });
     }
