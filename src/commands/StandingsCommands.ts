7296be8dd3c4c5f5e6739635bc1248e22efb377d
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2021-12-18 01:02:20 -0800

    V13 and slash commands (#21)
    * QOL updates and new command
    
    Model updates for API
    
    GetLastGame API call
    
    WIP of game recap command
    
    Other small fixes
    
    * fixed 'next game' format string and end game formatting
    
    * Added game recap link to title
    
    * Updated to v13
    
    * Command loading (arg) updates, intents, and command alignment / error handling
    
    * More slash command updates

--- src/commands/StandingsCommands.ts
+++ src/commands/StandingsCommands.ts
@@ -1,3 +1,4 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { Message, MessageEmbed } from "discord.js";
 import { Command } from "../models/Command";
 import { StandingsTypes } from "../models/StandingsTypes";
@@ -9,8 +10,8 @@ export const GetStandings: Command = {
 	name: 'standings',
     description: 'Get Standings. Standings type optional (`wildCard`, `divisionLeaders`)',
     help: 'standings divisionLeaders',
-	async execute(message: Message, args: string[]) {
-        const standingsArg = args?.[1];
+	async execute(message: Message, args?: string[]) {
+        const standingsArg = args?.[0];
         const standings = await API.Standings.GetStandings(standingsArg as StandingsTypes);
         
 		if(!standings?.[0]) {
@@ -29,7 +30,9 @@ export const GetStandings: Command = {
 			},
 			fields: standings.map(standing => {
                 return {
-                    name: `Conference: ${standing.conference.name}, Division: ${standing.division.name}`,
+                    name: standingsArg == StandingsTypes.WILDCARD
+					? `Conference: ${standing.conference.name}`
+					: `Conference: ${standing.conference.name}, Division: ${standing.division.name}`,
                     value: standing.teamRecords.map((teamRecord, i) =>
                         `${i+1}: ${teamRecord.team.name} - ${teamRecord.points} points  (${teamRecord.leagueRecord.wins}-${teamRecord.leagueRecord.losses}-${teamRecord.leagueRecord.ot})`
                     ).join(`\n`)
@@ -38,4 +41,40 @@ export const GetStandings: Command = {
 		});
 		message.channel.send({embeds: [embed]});
 	},
+	slashCommandDescription: () => {
+		return new SlashCommandBuilder()
+		.setName('standings')
+		.setDescription('Get NHL standings')
+		.addStringOption(option => {
+			return option.setName('type')
+				.setDescription('Optional standings type')
+				.addChoice('Wild Card', StandingsTypes.WILDCARD)
+				.addChoice('Divisional', StandingsTypes.DIVISION_LEADERS)
+			
+		})
+	},
+	executeSlashCommand: async (interaction) => {
+		const standingsArg = interaction.options.getString('type')
+        const standings = await API.Standings.GetStandings(standingsArg as StandingsTypes);
+		const embed = new MessageEmbed({
+			title: `Standings`,
+			description: `${standingsArg ? standingsArg : 'Regular Season By Conference'}`,
+			color: 111111,
+			footer: {
+				text: 'Source: NHL API',
+				iconURL: bot_thumbnail_image,
+			},
+			fields: standings.map(standing => {
+                return {
+                    name: standingsArg == StandingsTypes.WILDCARD
+					? `Conference: ${standing.conference.name}`
+					: `Conference: ${standing.conference.name}, Division: ${standing.division.name}`,
+                    value: standing.teamRecords.map((teamRecord, i) =>
+                        `${i+1}: ${teamRecord.team.name} - ${teamRecord.points} points  (${teamRecord.leagueRecord.wins}-${teamRecord.leagueRecord.losses}-${teamRecord.leagueRecord.ot})`
+                    ).join(`\n`)
+                }
+            })
+		});
+		interaction.reply({embeds: [embed]});
+	}
 }
--- src/models/Command.ts
+++ src/models/Command.ts
@@ -1,10 +1,23 @@
-import { Message } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CacheType, CommandInteraction, CommandInteractionOptionResolver, Message, PartialMessage } from "discord.js";
+
 
 //TODO - expand interface to have a 'canExecute' method to check args and return help message
 export interface Command {
     name: string;
+    adminOnly?: boolean;
     description: string;
     help?: string;
-    adminOnly?: boolean;
-    execute: (message: Message, args: string[]) => void;
+    execute: (message: Message, args?: string[]) => void;
+    slashCommandDescription?:  () => Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+    executeSlashCommand?: (options:  CommandInteraction) => void;
 }
+export interface CommandDictionary { [id: string]: Command }
+
+export interface ReactionCommand extends Command {
+    emojiId: string;
+    removeReaction?: boolean;
+    execute: (message: Message | PartialMessage) => void;
+}
+
+export interface ReactionCommandDictionary { [id: string]: ReactionCommand }
