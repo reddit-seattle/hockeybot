23f0df91267ed24b9c2ee2833a9450e8a22ae49d
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2025-04-05 11:16:28 -0700

    linting / cleanup

--- src/commands/Schedule.ts
+++ src/commands/Schedule.ts
@@ -1,102 +1,99 @@
-import {
-  EmbedBuilder,
-  SlashCommandBuilder,
-  SlashCommandSubcommandBuilder,
-} from "@discordjs/builders";
+import { EmbedBuilder, SlashCommandBuilder, SlashCommandSubcommandBuilder } from "@discordjs/builders";
+import { format, utcToZonedTime } from "date-fns-tz";
 import { Command } from "../models/Command";
 import { API } from "../service/API";
 import { Game as DayScheduleGame } from "../service/models/responses/DaySchedule";
-import { Game as TeamWeeklyScheduleGame } from "../service/models/responses/TeamWeeklyScheduleResponse";
 import { Game as TeamMonthlyScheduleGame } from "../service/models/responses/TeamMonthlyScheduleResponse";
-import { format, utcToZonedTime } from "date-fns-tz";
-import _ from "underscore";
-import { optionalDateOption, processLocalizedDateInput, relativeDateString, requiredTeamOption, teamOrPlayerAutocomplete, validTeamName } from "../utils/helpers";
+import { Game as TeamWeeklyScheduleGame } from "../service/models/responses/TeamWeeklyScheduleResponse";
 import { Config } from "../utils/constants";
+import {
+    optionalDateOption,
+    processLocalizedDateInput,
+    relativeDateString,
+    requiredTeamOption,
+    teamOrPlayerAutocomplete,
+    validTeamName,
+} from "../utils/helpers";
 
 const teamScheduleSubgroupCommand = new SlashCommandSubcommandBuilder()
-  .setName("team")
-  .setDescription("look up a team's schedule")
-  .addStringOption(requiredTeamOption)
-  .addStringOption((option) =>
-    option
-      .setName("type")
-      .setDescription("Weekly or Monthly")
-      .addChoices(
-        { name: "weekly", value: "weekly" },
-        { name: "monthly", value: "monthly" }
-      )
-  );
+    .setName("team")
+    .setDescription("look up a team's schedule")
+    .addStringOption(requiredTeamOption)
+    .addStringOption((option) =>
+        option
+            .setName("type")
+            .setDescription("Weekly or Monthly")
+            .addChoices({ name: "weekly", value: "weekly" }, { name: "monthly", value: "monthly" })
+    );
 const leagueScheduleSubgroupCommand = new SlashCommandSubcommandBuilder()
-  .setName("all")
-  .setDescription("see all games")
-  .addStringOption(optionalDateOption);
+    .setName("all")
+    .setDescription("see all games")
+    .addStringOption(optionalDateOption);
 
 export const GetSchedule: Command = {
-  name: "schedule",
-  description: "NHL Game Schedule",
-  slashCommandDescription: new SlashCommandBuilder()
-      .addSubcommand(leagueScheduleSubgroupCommand)
-      .addSubcommand(teamScheduleSubgroupCommand),
-  executeSlashCommand: async (interaction) => {
-    const subcommand = interaction.options.getSubcommand();
-    if (subcommand == "team") {
-      const team = interaction.options.getString("team", true);
-      if (!validTeamName(team)) {
-        interaction.reply("That's not an NHL team name buddy");
-        return;
-      }
-      await interaction.deferReply();
-      const monthly = interaction.options.getString("type") == "monthly";
-      const schedule = monthly
-        ? await API.Schedule.GetTeamMonthlySchedule(team)
-        : await API.Schedule.GetTeamWeeklySchedule(team);
-      await interaction.followUp({
-        embeds: [ScheduleEmbedBuilder(schedule, `${team} ${monthly ? 'Monthly' : 'Weekly'}`)],
-      });
-    } /*if (subcommand = 'all') */ else {
-      await interaction.deferReply();
-      const dateInput = interaction.options.getString("date", false);
-      const adjustedDate = processLocalizedDateInput(dateInput);
-      const schedule = await API.Schedule.GetDailySchedule(adjustedDate);
-      await interaction.followUp({
-        embeds: [ScheduleEmbedBuilder(schedule, format(adjustedDate ?? new Date(), Config.TITLE_DATE_FORMAT))],
-      });
-    }
-  },
-  autocomplete: teamOrPlayerAutocomplete,
+    name: "schedule",
+    description: "NHL Game Schedule",
+    slashCommandDescription: new SlashCommandBuilder()
+        .addSubcommand(leagueScheduleSubgroupCommand)
+        .addSubcommand(teamScheduleSubgroupCommand),
+    executeSlashCommand: async (interaction) => {
+        const subcommand = interaction.options.getSubcommand();
+        if (subcommand == "team") {
+            const team = interaction.options.getString("team", true);
+            if (!validTeamName(team)) {
+                interaction.reply("That's not an NHL team name buddy");
+                return;
+            }
+            await interaction.deferReply();
+            const monthly = interaction.options.getString("type") == "monthly";
+            const schedule = monthly
+                ? await API.Schedule.GetTeamMonthlySchedule(team)
+                : await API.Schedule.GetTeamWeeklySchedule(team);
+            await interaction.followUp({
+                embeds: [ScheduleEmbedBuilder(schedule, `${team} ${monthly ? "Monthly" : "Weekly"}`)],
+            });
+        } /*if (subcommand = 'all') */ else {
+            await interaction.deferReply();
+            const dateInput = interaction.options.getString("date", false);
+            const adjustedDate = processLocalizedDateInput(dateInput);
+            const schedule = await API.Schedule.GetDailySchedule(adjustedDate);
+            await interaction.followUp({
+                embeds: [ScheduleEmbedBuilder(schedule, format(adjustedDate ?? new Date(), Config.TITLE_DATE_FORMAT))],
+            });
+        }
+    },
+    autocomplete: teamOrPlayerAutocomplete,
 };
 
 // TODO - EmbedBuilders module
 const ScheduleEmbedBuilder = (
-  schedule: (
-    | DayScheduleGame
-    | TeamWeeklyScheduleGame
-    | TeamMonthlyScheduleGame
-  )[],
-  scheduleTypeDisplay: string
+    schedule: (DayScheduleGame | TeamWeeklyScheduleGame | TeamMonthlyScheduleGame)[],
+    scheduleTypeDisplay: string
 ) => {
-  return new EmbedBuilder().setTitle(`${scheduleTypeDisplay} Schedule`).addFields(
-    schedule.map((item) => {
-        const {startTimeUTC, venue, awayTeam, homeTeam} = item;
-        const dateSlug = relativeDateString(startTimeUTC);
-        const dateStr = `${format(utcToZonedTime(startTimeUTC, Config.TIME_ZONE), Config.BODY_DATE_FORMAT)} (${dateSlug})`;
-        const venuStr = `Venue: ${venue.default}`
-        let output = `${dateStr}\n${venuStr}`
-        // only show radio links if available
-        const {radioLink: awayAudio} = awayTeam;
-        const {radioLink: homeAudio} = homeTeam;
-        const homeRadioStr = homeAudio && `[${homeTeam.abbrev}](${homeAudio})`;
-        const awayRadioStr = awayAudio && `[${awayTeam.abbrev}](${awayAudio})`;
-        if(homeRadioStr || awayRadioStr){
-            output+= `\nListen: ${[homeRadioStr, awayRadioStr].filter(x => x != undefined).join(' - ')}`;
-        }
-        
+    return new EmbedBuilder().setTitle(`${scheduleTypeDisplay} Schedule`).addFields(
+        schedule.map((item) => {
+            const { startTimeUTC, venue, awayTeam, homeTeam } = item;
+            const dateSlug = relativeDateString(startTimeUTC);
+            const dateStr = `${format(
+                utcToZonedTime(startTimeUTC, Config.TIME_ZONE),
+                Config.BODY_DATE_FORMAT
+            )} (${dateSlug})`;
+            const venuStr = `Venue: ${venue.default}`;
+            let output = `${dateStr}\n${venuStr}`;
+            // only show radio links if available
+            const { radioLink: awayAudio } = awayTeam;
+            const { radioLink: homeAudio } = homeTeam;
+            const homeRadioStr = homeAudio && `[${homeTeam.abbrev}](${homeAudio})`;
+            const awayRadioStr = awayAudio && `[${awayTeam.abbrev}](${awayAudio})`;
+            if (homeRadioStr || awayRadioStr) {
+                output += `\nListen: ${[homeRadioStr, awayRadioStr].filter((x) => x != undefined).join(" - ")}`;
+            }
 
-      return {
-        name: `${awayTeam.abbrev} @ ${homeTeam.abbrev}`,
-        value: output,
-        inline: false,
-      };
-    })
-  );
+            return {
+                name: `${awayTeam.abbrev} @ ${homeTeam.abbrev}`,
+                value: output,
+                inline: false,
+            };
+        })
+    );
 };
--- src/commands/Standings.ts
+++ src/commands/Standings.ts
@@ -1,42 +1,32 @@
+import { EmbedBuilder } from "@discordjs/builders";
 import { SlashCommandBuilder } from "discord.js";
+import _ from "underscore";
 import { Command } from "../models/Command";
 import { API } from "../service/API";
-import { requiredConferenceOption, requiredDivisionOption, getOrdinal, teamOrPlayerAutocomplete, validTeamName } from "../utils/helpers";
-import { ConferenceAbbrev } from "../utils/enums";
 import { Standing } from "../service/models/responses/DefaultStandingsResponse";
-import { EmbedBuilder } from "@discordjs/builders";
-import _ from "underscore";
+import { ConferenceAbbrev } from "../utils/enums";
+import { requiredConferenceOption, requiredDivisionOption } from "../utils/helpers";
 
 /**
  * Standings
  * By League, Conference, Division, wildcard
  */
 export const GetStandings: Command = {
-    name: 'standings',
-    description: 'Get NHL Standings',
+    name: "standings",
+    description: "Get NHL Standings",
     slashCommandDescription: new SlashCommandBuilder()
-        .addSubcommand(cmd =>
-            cmd
-                .setName('division')
-                .setDescription('Division Standings')
-            .addStringOption(requiredDivisionOption)
+        .addSubcommand((cmd) =>
+            cmd.setName("division").setDescription("Division Standings").addStringOption(requiredDivisionOption)
         )
-        .addSubcommand(cmd =>
-            cmd
-                .setName('conference')
-                .setDescription('Conference Standings')
-                .addStringOption(requiredConferenceOption)
+        .addSubcommand((cmd) =>
+            cmd.setName("conference").setDescription("Conference Standings").addStringOption(requiredConferenceOption)
         )
         // .addSubcommand(cmd =>
         //     cmd
         //         .setName('league')
         //         .setDescription('League Standings')
         // )
-        .addSubcommand(cmd =>
-            cmd
-                .setName('wildcard')
-                .setDescription('Wildcard Standings')
-        ),
+        .addSubcommand((cmd) => cmd.setName("wildcard").setDescription("Wildcard Standings")),
 
     async executeSlashCommand(interaction) {
         await interaction.deferReply();
@@ -45,149 +35,141 @@ export const GetStandings: Command = {
         // if(subcommand == 'league') {
         //     // all standings
         // }
-        if(subcommand == 'conference') {
+        if (subcommand == "conference") {
             // standings by single conference
             const conference = interaction.options.getString("conference", true);
             const sortedStandings = standings
-                .filter(standing => standing.conferenceAbbrev == conference)
-                .sort(standing => standing.conferenceSequence)
-            
-            const title = `${conference === 'E' ? 'Eastern' : 'Western'} conference standings`
+                .filter((standing) => standing.conferenceAbbrev == conference)
+                .sort((standing) => standing.conferenceSequence);
+
+            const title = `${conference === "E" ? "Eastern" : "Western"} conference standings`;
             const fields = sortedStandings.map((standing, idx) => {
                 const { teamAbbrev, teamName } = standing;
-                const teamDescriptor = `${idx+1}: [${teamAbbrev.default}] ${teamName.default}`;
+                const teamDescriptor = `${idx + 1}: [${teamAbbrev.default}] ${teamName.default}`;
                 return {
                     name: teamDescriptor,
-                    value: buildTeamStandingString(standing)
-                }
+                    value: buildTeamStandingString(standing),
+                };
             });
             await interaction.followUp({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle(title)
-                        .addFields(fields)
-                ]
+                embeds: [new EmbedBuilder().setTitle(title).addFields(fields)],
             });
-
-        }
-        else if(subcommand == 'division') {
+        } else if (subcommand == "division") {
             // standings by single division
             const division = interaction.options.getString("division", true);
             const sortedStandings = standings
-                .filter(standing => standing.divisionAbbrev == division)
-                .sort(standing => standing.divisionSequence)
+                .filter((standing) => standing.divisionAbbrev == division)
+                .sort((standing) => standing.divisionSequence);
             const div = standings[0].divisionName;
-            const title = `${div} division standings`
+            const title = `${div} division standings`;
             const fields = sortedStandings.map((standing, idx) => {
                 const { teamAbbrev, teamName } = standing;
-                const teamDescriptor = `${idx+1}: [${teamAbbrev.default}] ${teamName.default}`;
+                const teamDescriptor = `${idx + 1}: [${teamAbbrev.default}] ${teamName.default}`;
                 return {
                     name: teamDescriptor,
-                    value: buildTeamStandingString(standing)
-                }
+                    value: buildTeamStandingString(standing),
+                };
             });
             await interaction.followUp({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle(title)
-                        .addFields(fields)
-                ]
-            })
-            
-            
-        }       
-        else if(subcommand == 'wildcard') {
+                embeds: [new EmbedBuilder().setTitle(title).addFields(fields)],
+            });
+        } else if (subcommand == "wildcard") {
             const eastern = standings
-                .filter(standing => standing.conferenceAbbrev == ConferenceAbbrev.eastern )
+                .filter((standing) => standing.conferenceAbbrev == ConferenceAbbrev.eastern)
                 .sort(wildcardSort);
-            
+
             const western = standings
-                .filter(standing => standing.conferenceAbbrev == ConferenceAbbrev.western)
+                .filter((standing) => standing.conferenceAbbrev == ConferenceAbbrev.western)
                 .sort(wildcardSort);
-            
+
             const easternPlayoffs = _.first(eastern, 8);
             const westernPlayoffs = _.first(western, 8);
 
-            const eastWildcardRace = eastern.filter(standing => standing.wildcardSequence > 2)
-            const westWildcardRace = western.filter(standing => standing.wildcardSequence > 2)
+            const eastWildcardRace = eastern.filter((standing) => standing.wildcardSequence > 2);
+            const westWildcardRace = western.filter((standing) => standing.wildcardSequence > 2);
 
-            const title = "Wildcard standings"
+            const title = "Wildcard standings";
             const fields = [
                 {
-                    name: 'Eastern Playoffs',
-                    value: easternPlayoffs.map((standing, idx) => {
-                        const pos = idx > 5 ? `WC${idx-5}` : idx+1;
-                        return `${pos}: ${buildWildcardString(standing)}`
-                    }).join('\n')
+                    name: "Eastern Playoffs",
+                    value: easternPlayoffs
+                        .map((standing, idx) => {
+                            const pos = idx > 5 ? `WC${idx - 5}` : idx + 1;
+                            return `${pos}: ${buildWildcardString(standing)}`;
+                        })
+                        .join("\n"),
                 },
                 {
-                    name: 'Eastern Wildcard',
-                    value: eastWildcardRace.map((standing) => {
-                        return `${buildWildcardString(standing)}`
-                    }).join('\n')
+                    name: "Eastern Wildcard",
+                    value: eastWildcardRace
+                        .map((standing) => {
+                            return `${buildWildcardString(standing)}`;
+                        })
+                        .join("\n"),
                 },
                 {
-                    name: 'Western Playoffs',
-                    value: westernPlayoffs.map((standing, idx) => {
-                        const pos = idx > 5 ? `WC${idx-5}` : idx+1;
-                        return `${pos}: ${buildWildcardString(standing)}`
-                    }).join('\n')
+                    name: "Western Playoffs",
+                    value: westernPlayoffs
+                        .map((standing, idx) => {
+                            const pos = idx > 5 ? `WC${idx - 5}` : idx + 1;
+                            return `${pos}: ${buildWildcardString(standing)}`;
+                        })
+                        .join("\n"),
                 },
                 {
-                    name: 'Western Wildcard',
-                    value: westWildcardRace.map((standing) => {
-                        return `${buildWildcardString(standing)}`
-                    }).join('\n')
+                    name: "Western Wildcard",
+                    value: westWildcardRace
+                        .map((standing) => {
+                            return `${buildWildcardString(standing)}`;
+                        })
+                        .join("\n"),
                 },
             ];
 
             await interaction.followUp({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle(title)
-                        .addFields(fields)
-                ]
-            })
-            
+                embeds: [new EmbedBuilder().setTitle(title).addFields(fields)],
+            });
         }
-    }
-}
+    },
+};
 
 const wildcardSort = (a: Standing, b: Standing) => {
-    return a.conferenceSequence - b.conferenceSequence || a.wildcardSequence - b.wildcardSequence
-}
+    return a.conferenceSequence - b.conferenceSequence || a.wildcardSequence - b.wildcardSequence;
+};
 
 const buildWildcardString = (standing: Standing) => {
     const {
-        teamAbbrev, teamName,
+        teamAbbrev,
+        teamName,
         points, // (82 pts [.500])
         gamesPlayed,
         divisionAbbrev,
-        divisionSequence
+        divisionSequence,
     } = standing;
 
     const teamDescriptor = `${teamAbbrev.default}`;
     const divisionDescriptor = `(${divisionAbbrev}${divisionSequence})`;
-    return `${teamDescriptor} - ${points} pts - ${gamesPlayed} GP - ${divisionDescriptor}`
-
-
-}
+    return `${teamDescriptor} - ${points} pts - ${gamesPlayed} GP - ${divisionDescriptor}`;
+};
 
 const buildTeamStandingString = (standing: Standing) => {
     const {
-        points, pointPctg, // (82 pts [.500])
-        wins, losses, otLosses, // (W-L-OTL)
-        streakCode, streakCount, // W2
-        l10Wins, l10Losses, l10OtLosses, // (L10)
+        points,
+        pointPctg, // (82 pts [.500])
+        wins,
+        losses,
+        otLosses, // (W-L-OTL)
+        streakCode,
+        streakCount, // W2
+        l10Wins,
+        l10Losses,
+        l10OtLosses, // (L10)
     } = standing;
-    
 
     const pointsDescriptor = `${points} pts`;
     const record = `(${wins}-${losses}-${otLosses})`;
-    const l10 = `(${l10Wins}-${l10Losses}-${l10OtLosses})`
-    const streak = `${streakCount}${streakCode}`
+    const l10 = `(${l10Wins}-${l10Losses}-${l10OtLosses})`;
+    const streak = `${streakCount}${streakCode}`;
 
     return `${pointsDescriptor} - ${record} - L10: ${l10} - streak ${streak}`;
-}
-
-
+};
